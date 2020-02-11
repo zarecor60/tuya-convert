c3a2cdf873cb90c535b979bf5eecca2d5d817488
Trey <zarecor60@gmail.com>
2020-02-11 08:15:44 -0600

    Converted install_prereq.sh to Fedora requirements
    Began development  of fedora support - beginning with getting correct prerequisites

--- install_prereq.sh
+++ install_prereq.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
+sudo dnf update
+# sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
+sudo dnf install -y git iw dnsmasq hostapd screen curl make automake gcc gcc-c++ kernel-devel python3-pip python3-setuptools python3-wheel python3-devel mosquitto haveged net-tools openssl-devel
 
 sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
 
