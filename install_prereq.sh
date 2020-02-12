5bec1a034a144e8056bdab4d87344133efc3d5ae
Trey <zarecor60@gmail.com>
2020-02-12 08:57:41 -0500

    Fix inverted values
    Fixed inverted values used for determining if the install operation passed through or not

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dist=$(lsb_release -is)
+pkgInstalled=1;
 
 set -e
 
@@ -8,16 +9,16 @@ if [ $dist == 'Ubuntu' ]
 then
 	sudo apt-get update
 	sudo apt-get install -y git iw dnsmasq hotapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev python3 mosquitto haveged net-tools libssl-dev
-	pkgInstalled=1;
+	pkgInstalled=0;
 elif [ $dist == 'Fedora' ]
 then
 	sudo dnf update
 	sudo dnf install -y git iw dnsmasq hostapd screen curl make automake gcc gcc-c++ kernel-devel python3-pip python3-setuptools python3-wheel python3-devel mosquitto haveged net-tools openssl-devel
-	pkgInstalled=1;
+	pkgInstalled=0;
 fi
 sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
 
-if [ pkgInstalled -eq 1 ]
+if [ $pkgInstalled -eq 0 ]
 then
 	echo "Ready to start upgrade"
 else
